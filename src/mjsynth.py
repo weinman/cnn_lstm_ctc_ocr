77db194ba7a17613b9837ab7b94d6e027aeabc04
Ben Gafford <gaffordb@grinnell.edu>
2018-06-21 11:14:39 -0500

    Snapshot with _text_to_labels method of getting labels

--- src/mjsynth.py
+++ src/mjsynth.py
@@ -17,11 +17,13 @@
 import os
 import tensorflow as tf
 import numpy as np
+from map_generator import data_generator
 
 # The list (well, string) of valid output characters
 # If any example contains a character not found here, an error will result
 # from the calls to .index in the decoder below
 out_charset="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+out_charset_tf=tf.string_split([tf.constant("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")], "")
 
 def num_classes():
     return len(out_charset)
@@ -92,16 +94,14 @@ def threaded_input_pipeline(base_dir,file_patterns,
         dataset = dataset.map(_parse_function,
                               num_parallel_calls=num_threads)
     
-    with tf.device(batch_device): # Create batch queue
+    with tf.device(batch_device): # Create batch
 
+        # Hack -- probably a better way to do this! Just want dynamic padding!
+        # Pad batches to max data size (bucketing it all into the same bucket)
         dataset = dataset.apply(tf.contrib.data.bucket_by_sequence_length
                                 (element_length_func=_element_length_fn,
-                                 bucket_batch_sizes=np.full
-                                 (len([32, 64, 96, 128, 160, 192, 224, 256]) + 1, batch_size),
-                                 bucket_boundaries=[32, 64, 96, 128, 160, 192, 224, 256]))
-        
-
-        #dataset = dataset.padded_batch(batch_size, )
+                                 bucket_batch_sizes=[batch_size],
+                                 bucket_boundaries=[]))
 
         dataset = dataset.map(lambda image, 
                               width, label, 
@@ -115,6 +115,9 @@ def threaded_input_pipeline(base_dir,file_patterns,
 
 def _element_length_fn(image, width, label, length, text, filename):
     return width
+
+def dataset_element_length_fn(_, image):
+    return tf.shape(image)[2]
 
 def _get_input_filter(width, width_threshold, length, length_threshold):
     """Boolean op for discarding input data based on string or image size
@@ -149,52 +152,63 @@ def _get_input_filter(width, width_threshold, length, length_threshold):
 
     return keep_input
 
-def _get_dataset(base_dir, file_patterns=['*.tfrecord']):
-    """Get a data queue for a list of record files"""
+def _get_dataset():
+    """Get a dataset from generator"""
+    return tf.data.Dataset.from_generator(data_generator, 
+                                          (tf.string, tf.int32),
+                                          (tf.TensorShape([]), 
+                                           (tf.TensorShape((32, None, 3)))))
+    return dataset
 
-    # List of lists ...
-    data_files = [tf.gfile.Glob(os.path.join(base_dir,file_pattern))
-                  for file_pattern in file_patterns]
-    # flatten
-    data_files = [data_file for sublist in data_files for data_file in sublist]
+def _text_to_labels(text):
+    """Convert given text (tf.string) into a list of tf.int32's"""
+    labels = tf.data.Dataset.from_tensor_slices(tf.string_split([text],""))
+    
+    # Converts ['A', 'B', 'C'] -> [0, 1, 2]
+    # Note: MUST RUN tf.tables_initializer().run() in order for this to work
+    table = tf.contrib.lookup.index_table_from_tensor(mapping=out_charset_tf,
+                                                      num_oov_buckets=1,
+                                                      default_value=-1)
+    labels = table.lookup(labels)
+    
+    return labels
 
-    # feed filenames for processing
-    dataset = tf.data.TFRecordDataset(data_files)
+def _preprocess_dataset(caption, image):
+    """Get everything how it should be"""
+
+    #NOTE: final image should be pre-grayed by opencv *before* generation
+    image = tf.image.rgb_to_grayscale(image) 
+    image = _preprocess_image(image)
+
+    width = tf.size(image[1]) 
+    labels = _text_to_labels(caption)  
+    length = tf.size(labels)
+    text = caption
+    return image, width, label, length, text
+                
+
+def _parse_function(dataset, num_threads):
+    """Parse the elements of the dataset"""
+    
+    # Format elements appropriately
+    dataset = dataset.map(_preprocess_dataset, num_parallel_calls=num_threads)
 
     return dataset
 
-# https://www.tensorflow.org/programmers_guide/datasets#consuming_tfrecord_data
-def _parse_function(data):
-    """Parse the elements of the dataset"""
 
-    feature_map = {
-        'image/encoded':  tf.FixedLenFeature( [], dtype=tf.string, 
-                                              default_value='' ),
-        'image/labels':   tf.VarLenFeature( dtype=tf.int64 ), 
-        'image/width':    tf.FixedLenFeature( [1], dtype=tf.int64,
-                                              default_value=1 ),
-        'image/filename': tf.FixedLenFeature([], dtype=tf.string,
-                                             default_value='' ),
-        'text/string':     tf.FixedLenFeature([], dtype=tf.string,
-                                              default_value='' ),
-        'text/length':    tf.FixedLenFeature( [1], dtype=tf.int64,
-                                              default_value=1 )
-    }
+
+def _char_to_int(character):
+    """Convert given character (really tf.string of length 1) to its integer representation from out_charset"""
     
-    features = tf.parse_single_example(data, feature_map)
-    
-    # Initialize fields according to feature map
-    image = tf.image.decode_jpeg( features['image/encoded'], channels=1 ) #gray
-    width = tf.cast( features['image/width'], tf.int32) # for ctc_loss
-    label = tf.serialize_sparse( features['image/labels'] ) # for batching
-    length = features['text/length']
-    text = features['text/string']
-    filename = features['image/filename']
+    tf.contrib.lookup.string_to_index(character, out_charset_tf) # default val -1
+    return out_charset.index(character)
 
-    # Prepare image
-    image = _preprocess_image(image)
+def _generator_wrapper():
+    caption, image = data_generator()
 
-    return image,width,label,length,text,filename
+    # Transform string text to sequence of indices using charset
+    labels = [out_charset.index(c) for c in list(caption)
+    yield caption, image, labels
 
 def _preprocess_image(image):
     # Rescale from uint8([0,255]) to float([-0.5,0.5])
