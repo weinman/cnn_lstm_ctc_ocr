a6a2e246a74d598777cc4716a7cf65ae1ceba3ba
Jerod Weinman <jerod@acm.org>
2018-09-04 10:37:13 -0500

    Added documentation and renamed some variables

--- src/lexicon.py
+++ src/lexicon.py
@@ -23,25 +23,50 @@ import numpy as np
 import tensorflow as tf
 
 def read_dict(fname):
-    f = open(fname, 'r')
-    # for each line, return line, init_caps, all_caps
-    # Ex: line="  abc " -> ("abc", "Abc", "ABC")
-    vocab = list(chain.from_iterable((line.strip(), line.strip().title(), 
-				 line.strip().upper()) for line in f))
+    """Read lexicon entries from a file (one per line), adding initial
+    capitalization and all uppercase transformations to each entry
+
+    """
+    with open(fname, 'r') as fd:
+        # for each line, return line, init_caps, all_caps
+        # Ex: line="  abc " -> ("abc", "Abc", "ABC")
+        vocab = list(chain.from_iterable((line.strip(), line.strip().title(), 
+				          line.strip().upper()) for line in fd))
     return vocab 
 
-def dictionary_from_file(fname, char_string):
+
+def dictionary_from_file(fname, charset):
+    """Create a label-indexed version of the lexicon from a lexicon file name.
+    Parameters:
+      fname   : path to the file name containing the lexicon entries
+      charset : string containing one instance of each valid character
+    Returns:
+      tensor_dict : A tf.SparseTensor with one row per lexicon entry and 
+                    columns containing indices of corresponding chracters
+                    in charset.
+    """
     vocab = read_dict(fname)
-    return dictionary_from_list(vocab, char_string)
-   
-def dictionary_from_list(vocab, char_string):
-    # inds are all non-zero char values
+    return dictionary_from_list(vocab, charset)
+
+
+def dictionary_from_list(vocab, charset):
+    """Create a label-indexed version of the lexicon from a list of strings.
+    Parameters:
+      vocab   : list of strings in the lexicon
+      charset : string containing one instance of each valid character
+    Returns:
+      tensor_dict : A tf.SparseTensor with one row per lexicon entry and 
+                    columns containing indices of corresponding chracters
+                    in charset.
+    """
+
+    # inds are locations of valid character values
     inds = np.array(
             [[i, j] for i,word in enumerate(vocab) for j in range(len(word))],
             dtype=np.int32)
-    # parse each character label using char_string as index reference
+    # parse each character label using charset as index reference
     vals = np.array(
-            [char_string.index(c) for word in vocab for c in word], 
+            [charset.index(ch) for word in vocab for ch in word], 
 	    dtype=np.int32)
     dims = np.array(
             [len(vocab), max(map(lambda x: len(x), vocab))], dtype=np.int32)
